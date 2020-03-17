a32742f5fd0d81ec19d81ba392d10a53a0b3008a
svetlana <s.ananke.s@gmail.com>
2020-03-17 15:44:26 +0300

    Перенос вспомогательных методов из тестового класса в файле test_add_group.py в отдельный класс Application в файл application.py

---
+++ application.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+
+class Application:
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.vars = {}
+
+    def logout(self):
+        self.driver.find_element(By.LINK_TEXT, "Logout").click()
+
+    def return_to_groups_page(self):
+        self.driver.find_element(By.LINK_TEXT, "groups").click()
+
+    def create_group(self, group):
+        self.open_groups_page()
+        # init group creation
+        self.driver.find_element(By.XPATH, "(//input[@name=\'new\'])[2]").click()
+        # fill group form
+        self.driver.find_element(By.NAME, "group_name").send_keys(group.name)
+        self.driver.find_element(By.NAME, "group_header").send_keys(group.header)
+        self.driver.find_element(By.NAME, "group_footer").send_keys(group.footer)
+        # submit group creation
+        self.driver.find_element(By.NAME, "submit").click()
+        # return_to_groups_page
+        self.return_to_groups_page()
+
+    def open_groups_page(self):
+        self.driver.find_element(By.LINK_TEXT, "groups").click()
+
+    def login(self, username, password):
+        self.open_home_page()
+        self.driver.find_element(By.NAME, "user").send_keys(username)
+        self.driver.find_element(By.NAME, "pass").send_keys(password)
+        self.driver.find_element(By.XPATH, "//input[@value=\'Login\']").click()
+
+    def open_home_page(self):
+        self.driver.get("http://localhost/addressbook/")
+        self.driver.set_window_size(1936, 1056)
+
+    def destroy(self):
+        self.driver.quit()
--- test_add_group.py
+++ test_add_group.py
@@ -2,58 +2,26 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from group import Group
+from application import Application
 
-class TestTestaddgroup():
+
+class Testaddgroup():
   def setup_method(self, method):
-    self.driver = webdriver.Chrome()
-    self.vars = {}
+    self.app = Application()
   
-  def teardown_method(self, method):
-    self.driver.quit()
-
 
   def test_add_group(self):
-    self.login(username="admin", password="secret")
-    self.create_group(Group(name="new2", header="new2", footer="new2"))
-    self.logout()
-    self.driver.close()
+
+    self.app.login(username="admin", password="secret")
+    self.app.create_group(Group(name="new2", header="new2", footer="new2"))
+    self.app.logout()
 
 
   def test_add_empty_group(self):
-    self.login(username="admin", password="secret")
-    self.create_group(Group(name="", header="", footer=""))
-    self.logout()
-    self.driver.close()
+    self.app.login(username="admin", password="secret")
+    self.app.create_group(Group(name="", header="", footer=""))
+    self.app.logout()
 
 
-  def logout(self):
-    self.driver.find_element(By.LINK_TEXT, "Logout").click()
-
-  def return_to_groups_page(self):
-    self.driver.find_element(By.LINK_TEXT, "groups").click()
-
-  def create_group(self, group):
-    self.open_groups_page()
-    # init group creation
-    self.driver.find_element(By.XPATH, "(//input[@name=\'new\'])[2]").click()
-    # fill group form
-    self.driver.find_element(By.NAME, "group_name").send_keys(group.name)
-    self.driver.find_element(By.NAME, "group_header").send_keys(group.header)
-    self.driver.find_element(By.NAME, "group_footer").send_keys(group.footer)
-    # submit group creation
-    self.driver.find_element(By.NAME, "submit").click()
-    # return_to_groups_page
-    self.return_to_groups_page()
-
-  def open_groups_page(self):
-    self.driver.find_element(By.LINK_TEXT, "groups").click()
-
-  def login(self, username, password):
-    self.open_home_page()
-    self.driver.find_element(By.NAME, "user").send_keys(username)
-    self.driver.find_element(By.NAME, "pass").send_keys(password)
-    self.driver.find_element(By.XPATH, "//input[@value=\'Login\']").click()
-
-  def open_home_page(self):
-    self.driver.get("http://localhost/addressbook/")
-    self.driver.set_window_size(1936, 1056)
+  def teardown_method(self, method):
+    self.app.destroy()
